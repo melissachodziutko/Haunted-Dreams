d512fbf86219503acd9ec3ad4fa1b84514ffabbd
alykkehoy <anderslykkehoy@gmail.com>
2020-10-25 17:44:23 -0400

    added falling through floors and ramps

---
+++ Assets/Scripts/DropThrough.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropThrough : MonoBehaviour
+{
+    private PlatformEffector2D effector;
+    private float fallDelay = 0.25f;
+    private float waitTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+        waitTime = fallDelay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.S)){
+            effector.rotationalOffset = 0f;  
+            waitTime = fallDelay;          
+        }
+        if(Input.GetKey(KeyCode.S)){
+            if(waitTime <= 0){
+                effector.rotationalOffset = 180f;
+                waitTime = fallDelay;
+            }else{
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
